32729247a3d5886ec8f4a4fa480a106a21c52b03
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-11-10 13:01:43 +0000

    hw2

--- Assets/Scripts/DiamondScript.cs
+++ Assets/Scripts/DiamondScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,41 @@ using UnityEngine;
 public class DiamondScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private Rigidbody2D rb2d; 
     void Start()
     {
-        Debug.Log("Hello world!");
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb2d.AddForce(Vector2.up * 5);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddForce(Vector2.left * 5);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddForce(Vector2.right * 5);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb2d.AddForce(Vector2.down * 5);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb2d.AddTorque(5);
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            rb2d.angularVelocity = 0;
+            rb2d.velocity.Set(0,0);
+        }
+
     }
 }
